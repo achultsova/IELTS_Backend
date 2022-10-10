47a26e15c86fe028e221f0b43bc003fcd36fcbee
achultsova <nchulczova@gmail.com>
2022-10-10 16:35:04 +0200

    feat: add validation messages

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,13 +6,18 @@ const userController = require('../controllers/user-controller');
 
 
 router.post('/registration',
-    body('name').notEmpty(),
-    body('surname').notEmpty(),
-    body('email').isEmail(),
-    body('password').isLength({min: 8, max: 64}),
+    body('name').notEmpty().isLength({min: 1, max: 30}).withMessage('Name length must be between 1 and 30 symbols')
+        .matches(/^[A-Za-z\s]+$/).withMessage('Name can contain only letters'),
+    body('surname').notEmpty().isLength({min: 1, max: 30}).withMessage('Name length must be between 1 and 30 symbols')
+        .matches(/^[A-Za-z\s]+$/).withMessage('Name can contain only letters'),
+    body('email').isEmail().isLength({max: 256}).withMessage('Email length must be no more than 256 symbols'),
+    body('password').isEmail().isLength({max: 256}).withMessage('Email length must be no more than 256 symbols'),
     userController.registration
 );
-router.post('/signin', userController.login);
+router.post('/signin',
+    body('email').isEmail().isLength({max: 256}).withMessage('Email length must be no more than 256 symbols'),
+    body('password').isEmail().isLength({max: 256}).withMessage('Email length must be no more than 256 symbols'),
+    userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
