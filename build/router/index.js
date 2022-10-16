9738c90a881093a1c6e2ab34fd7baeb3d85e2b08
achultsova <nchulczova@gmail.com>
2022-10-16 19:56:45 +0200

    feat: validation on backend side

--- build/router/index.js
+++ build/router/index.js
@@ -6,8 +6,10 @@ var router = express_1.default.Router();
 var body = require('express-validator').body;
 var authMiddleware = require('../middlewares/auth-middleware');
 var userController = require('../controllers/user-controller');
-router.post('/registration', body('name').notEmpty(), body('surname').notEmpty(), body('email').isEmail(), body('password').isLength({ min: 8, max: 64 }), userController.registration);
-router.post('/signin', userController.login);
+router.post('/registration', body('name').notEmpty().isLength({ min: 1, max: 30 }).withMessage('Name length must be between 1 and 30 symbols')
+    .matches(/^[A-Za-z\s]+$/).withMessage('Name can contain only letters'), body('surname').notEmpty().isLength({ min: 1, max: 30 }).withMessage('Name length must be between 1 and 30 symbols')
+    .matches(/^[A-Za-z\s]+$/).withMessage('Name can contain only letters'), body('email').isEmail().isLength({ max: 256 }).withMessage('Email length must be no more than 256 symbols'), body('password').isEmail().isLength({ max: 256 }).withMessage('Email length must be no more than 256 symbols'), userController.registration);
+router.post('/signin', body('email').isEmail().isLength({ max: 256 }).withMessage('Email length must be no more than 256 symbols'), body('password').isEmail().isLength({ max: 256 }).withMessage('Email length must be no more than 256 symbols'), userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
